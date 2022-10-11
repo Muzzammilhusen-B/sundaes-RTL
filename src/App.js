fdfabfd8fd0d7ced3f28a1e440b793fa568b62fa
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2022-10-11 09:39:38 +0530

    ESlint, Prettier and Bootstarp setting

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import {useState} from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [checked1, setChecked1] = useState(false);
-  const [checked2, setChecked2] = useState(false);
-
-  const newButtonColor1 = checked1 ? "red" : "";
-  const newButtonColor2 = checked2 ? "green" : "";
-
-  return (
-    <div>
-      <div>
-        <span>
-          <input
-            type="checkbox"
-            id="red-checkbox"
-            onChange={(e) => setChecked1(e.target.checked)}
-            defaultChecked={checked1}
-          />
-          <label htmlFor="red-checkbox">Red</label>
-        </span>
-        <span>
-          <input
-            type="checkbox"
-            id="green-checkbox"
-            onChange={(e) => setChecked2(e.target.checked)}
-            defaultChecked={checked2}
-          />
-          <label htmlFor="green-checkbox">Green</label>
-        </span>
-      </div>
-      <div>
-        <button style={{backgroundColor: newButtonColor1}}>Button1</button>
-        <button style={{backgroundColor: newButtonColor2}}>Button2</button>
-      </div>
-    </div>
-  );
+  return <div />;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,46 +2,4 @@
 import {fireEvent, render, screen} from "@testing-library/react";
 import App from "./App";
 
-describe("Starting", () => {
-  test("initial button and checkbox", () => {
-    render(<App />);
-    const redCheckbox = screen.getByRole("checkbox", {name: "Red"});
-
-    const greenCheckbox = screen.getByRole("checkbox", {
-      name: "Green",
-    });
-
-    const button1 = screen.getByRole("button", {name: "Button1"});
-    const button2 = screen.getByRole("button", {name: "Button2"});
-
-    // screen.debug();
-    // expect(redCheckbox).toHaveTextContent("Change to green");
-  });
-
-  test("testing fire event for chebox checked and button color for red", () => {
-    render(<App />);
-    const redCheckbox = screen.getByRole("checkbox", {name: "Red"});
-
-    const button1 = screen.getByRole("button", {name: "Button1"});
-
-    fireEvent.click(redCheckbox);
-
-    expect(redCheckbox).toBeChecked();
-
-    expect(button1).toHaveStyle({backgroundColor: "red"});
-  });
-  test("testing fire event for chebox checked and button color for green", () => {
-    render(<App />);
-
-    const greenCheckbox = screen.getByRole("checkbox", {
-      name: "Green",
-    });
-
-    const button2 = screen.getByRole("button", {name: "Button2"});
-    fireEvent.click(greenCheckbox);
-
-    expect(greenCheckbox).toBeChecked();
-
-    expect(button2).toHaveStyle({backgroundColor: "green"});
-  });
-});
+test("1", () => {});
