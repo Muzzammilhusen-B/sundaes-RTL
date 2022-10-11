03f107b4bfa97490b7a0df5ae0d861235b446196
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2022-10-11 09:07:48 +0530

    Test give from Rohan for simple chekbox and button color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+
+  const newButtonColor1 = checked1 ? "red" : "";
+  const newButtonColor2 = checked2 ? "green" : "";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>
+          <input
+            type="checkbox"
+            id="red-checkbox"
+            onChange={(e) => setChecked1(e.target.checked)}
+            defaultChecked={checked1}
+          />
+          <label htmlFor="red-checkbox">Red</label>
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            id="green-checkbox"
+            onChange={(e) => setChecked2(e.target.checked)}
+            defaultChecked={checked2}
+          />
+          <label htmlFor="green-checkbox">Green</label>
+        </span>
+      </div>
+      <div>
+        <button style={{backgroundColor: newButtonColor1}}>Button1</button>
+        <button style={{backgroundColor: newButtonColor2}}>Button2</button>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable no-unused-vars */
+import {fireEvent, render, screen} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Starting", () => {
+  test("initial button and checkbox", () => {
+    render(<App />);
+    const redCheckbox = screen.getByRole("checkbox", {name: "Red"});
+
+    const greenCheckbox = screen.getByRole("checkbox", {
+      name: "Green",
+    });
+
+    const button1 = screen.getByRole("button", {name: "Button1"});
+    const button2 = screen.getByRole("button", {name: "Button2"});
+
+    // screen.debug();
+    // expect(redCheckbox).toHaveTextContent("Change to green");
+  });
+
+  test("testing fire event for chebox checked and button color for red", () => {
+    render(<App />);
+    const redCheckbox = screen.getByRole("checkbox", {name: "Red"});
+
+    const button1 = screen.getByRole("button", {name: "Button1"});
+
+    fireEvent.click(redCheckbox);
+
+    expect(redCheckbox).toBeChecked();
+
+    expect(button1).toHaveStyle({backgroundColor: "red"});
+  });
+  test("testing fire event for chebox checked and button color for green", () => {
+    render(<App />);
+
+    const greenCheckbox = screen.getByRole("checkbox", {
+      name: "Green",
+    });
+
+    const button2 = screen.getByRole("button", {name: "Button2"});
+    fireEvent.click(greenCheckbox);
+
+    expect(greenCheckbox).toBeChecked();
+
+    expect(button2).toHaveStyle({backgroundColor: "green"});
+  });
 });
