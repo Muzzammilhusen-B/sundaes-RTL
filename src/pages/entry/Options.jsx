727666a55445cbbc61703fe70954c79adc3d808f
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2022-10-17 18:24:02 +0530

    Code and test scoop option images from server

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+import {useEffect, useState} from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOptions from "./ScoopOptions";
+
+export default function Options({optionType}) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        //TODO: handle error
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+
+  const optionItem = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItem}</Row>;
+}
