e7e66a917164ff6e4cfc426394eb380d3c4eddcd
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2023-01-11 11:57:23 +0530

    scoop options value validation test

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -6,6 +6,7 @@ import {
 import {OrderEntry} from "../OrderEntry";
 import {rest} from "msw";
 import {server} from "../../../mocks/server";
+import userEvent from "@testing-library/user-event";
 
 test("Handle error for scoop and toppings route", async () => {
   server.resetHandlers(
@@ -24,4 +25,24 @@ test("Handle error for scoop and toppings route", async () => {
 
     expect(alerts).toHaveLength(2);
   });
+});
+
+test("disable order button if there are no scoops ordered", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  //order button should be disabled at first, even before option load
+  const orderButton = screen.getByRole("button", {name: /order sundae/i});
+  expect(orderButton).toBeDisabled();
+
+  //expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole("spinbutton", {name: "Vanilla"});
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  //expect button to be disabled after removing scoop
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
